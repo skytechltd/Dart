157bf7d068e91c588f654db890ba3c75e8fcb804
david <climber247@gmail.com>
2021-10-01 17:03:00 +0100

    Restructured classes

--- Benchmarking/runsim.py
+++ Benchmarking/runsim.py
@@ -5,6 +5,8 @@ import logging
 import time
 import pprint
 import math
+import json
+import copy
 from scipy.spatial import distance
 from enum import Enum, unique
 
@@ -13,6 +15,7 @@ sys.path.insert(0, '../DAA')
 import daa_opticalflow as daa
 
 
+DEFAULT_SETUPS='setups.json'
 RES_FILE='daares.csv'
 
 #
@@ -32,7 +35,7 @@ RES_FILE='daares.csv'
 #
 #   * Support scenarios, 0 a flight - reject FP, 1 single collision, 2 multiple
 #
-#   * PAwn to seek drone, not just assume its at waypoint - important for smaller pawns
+#   * Pawn to seek drone, not just assume its at waypoint - important for smaller pawns
 #
 #
 #
@@ -50,34 +53,46 @@ class Detection(Enum):
     TRUE = 1
     FALSE = -1
 
-class Pos:
-    def __init__(self,x,y,z):
-        self.x=x
-        self.y=y
-        self.z=z   
 
-class WayPoint:
-    def __init__(self,s,e,v):
-        self.start=s
-        self.end=e
-        self.v=v
+#class WayPoint:
+    #def __init__(self,s,e,v):
+        #self.start=aims)
+       # self.end=Pos(e)
+        #self.v=v
         #self.pos3darray=[x,y,z]
+
+class AirVehicle:
+    ACC_DELAY = 2 # Small delay until vehicle gets up to speed then assume t=d*s
+    def __init__(self,n,s,distance=0):
+        self.name = n
+        # UE4 global position
+        self.origin = airsim.Vector3r(s['X'],s['Y'],s['Z'])
+        # Offsets from origin
+        self.offset = airsim.Vector3r(s['X'],s['Y'],s['Z'])
+        self.timeout = 3e+38 # Default, timeout to prevent vehicles getting stuck on terrain
+        self.distance = distance
+        self.approach = [0,0]
+
+    def setTrajectory(self,a,b,v,yaw=0):
+        self.start = airsim.Vector3r(a[0],a[1],a[2]) - self.offset
+        self.end =   airsim.Vector3r(b[0],b[1],b[2]) - self.offset
+        self.v = v
+        self.yaw = -(yaw-90)
+        if self.distance>0:
+            self.timeout = (self.distance*v) + AirVehicle.ACC_DELAY
 
 
 # Check for a collision event and hold value
 class Collision:
-    def __init__(self,r,e):
-        self.detection_range = r
-        self.detection_hit = e
+    def __init__(self,s):
+        #self.pawnName = pn
+        #self.approach = a
+        self.setup=s
+        self.detection_range = s['pawn_sdist']
+        self.detection_hit = s['coll_dist']
         self.distance = math.inf
         self.hascollided = False
         self.inrange = False
-
-    def setDrone(self,d):
-        self.drone = d
-
-    def setPawn(self,p):
-        self.pawn = p
 
 
     def update(self,d,p):
@@ -91,165 +106,250 @@ class Collision:
 
         if not self.inrange:
             self.inrange = True if self.distance < self.detection_range else False
-            #print("Comapre: ",self.distance," to ",self.detection_range)
    
 
 
-    # Other distance metrics are available https://docs.scipy.org/doc/scipy/reference/spatial.html
-    #def collision(self,d,p,r):
-    #    print ("Distance: %f.1" % (distance.euclidean(p,self.start.pos3darray)))
-    #    if distance.euclidean(d,p) < r:
-     #       return True
-     #   else:
-     #       return False
+    # Collide the drone and pawn
+    def start(self,cl,drone,pawn):
+
+       
+        cl.enableApiControl(True, "Drone")
+        cl.enableApiControl(True, pawn.name)
+ 
+        # Arm, takeoff and move to position, would be better to teleport but they just fall to the ground
+        cl.armDisarm(True, "Drone") 
+        cl.armDisarm(True, pawn.name)
+
+        # Take off
+        dt = cl.takeoffAsync(vehicle_name="Drone")
+        pt = cl.takeoffAsync(vehicle_name=pawn.name)
+        dt.join()
+        pt.join()
+
+
+        # Shift to start positions
+        #print("Drone SPos(%.1f,%.1f,%.1f), V:%d"%(drone.start.x_val, drone.start.y_val, drone.start.z_val,drone.v))
+        #print("Pawn  SPos(%.1f,%.1f,%.1f), V:%d, Y:%d"%(pawn.start.x_val, pawn.start.y_val, pawn.start.z_val,pawn.v,pawn.yaw))
+        dt = cl.moveToPositionAsync(drone.start.x_val, drone.start.y_val, drone.start.z_val, drone.v, \
+                                            vehicle_name="Drone", timeout_sec=drone.timeout)
+        pt = cl.moveToPositionAsync(pawn.start.x_val, pawn.start.y_val, pawn.start.z_val, pawn.v,  \
+                                            vehicle_name=pawn.name, timeout_sec=pawn.timeout)
+        dt.join()
+        pt.join()
+
+        # Let drone settle as global motion from terrain is throwing false positive with optical flow first round
+        print("Sleeping")
+        #time.sleep(10)
+        # Debug
+        #airsim.wait_key('Press any key to collide')
 
 
 
-def start(collision):
-
-    #drone,pawn=args
-
-    #collision=Collision(20,5)
-
-    try:
-       client = airsim.MultirotorClient()
-       client.confirmConnection()
-    except Exception:
-        print("Could not connect to AirSim, is it running?")
-        return
-
-    #if len(client.simListSceneObjects('Pawn'))==0:
-    #    if not client.simAddVehicle("Pawn","simpleflight",airsim.Pose(),"SmallBalloon"):
-    #        print('Error crearting Pawn')   
-    #client.simDestroyObject("Pawn") # leasds to segfault
-
-    drone=collision.drone
-    pawn=collision.pawn
-
-    client.enableApiControl(True, "Drone")
-    client.enableApiControl(True, "Pawn")
-
-    # Arm, takeoff and move to position, would be better to teleport but they just fall to the ground
-    client.armDisarm(True, "Drone") 
-    client.armDisarm(True, "Pawn")
-
-    # Take off
-    dt = client.takeoffAsync(vehicle_name="Drone")
-    pt = client.takeoffAsync(vehicle_name="Pawn")
-    dt.join()
-    pt.join()
-
-    # Shift to start positions
-    dt = client.moveToPositionAsync(drone.start.x, drone.start.y, drone.start.z, drone.v, vehicle_name="Drone")
-    pt = client.moveToPositionAsync(pawn.start.x, pawn.start.y, pawn.start.z, pawn.v, vehicle_name="Pawn")
-    dt.join()
-    pt.join()
-
-    # Let drone settle as global motion from terrain is throwing false positive with optical flow first round
-    print("Sleeping")
-    time.sleep(10)
-    # Debug
-    #airsim.wait_key('Press any key to collide')
-
-
-
-    # Two approaches, we either:
-    # A. Move the pawn to collide with the drone (same position and check) for collision, 
-    #    assuming another script is responsible for a DAA manoeuvre, but airsim doesn't like
-    #    two threads accessing the client api and UE4 crashes. Or
-    # B. Move the pawn to nearly collide with the drone, run DAA on a thread and listen for a collision warning
-    #    we can record this along with the distance and mark as a fair true detection, false alarm or missed 
-
-
-    # For HITL DAA use simGetCollision
-    # Confirm collision? Old way - not alway reliable
-    #try:
-    #    collision = client.simGetCollisionInfo(vehicle_name="Drone2")
-    #    if collision.has_collided:
-    #        printunrealengine NewMap is generated
+        # Two approaches, we either:
+        # A. Move the pawn to collide with the drone (same position and check) for collision, 
+        #    assuming another script is responsible for a DAA manoeuvre, but airsim doesn't like
+        #    two threads accessing the client api and UE4 crashes. Or
+        # B. Move the pawn to nearly collide with the drone, run DAA on a thread and listen for a collision warning
+        #    we can record this along with t print("Running simulation for %s %d of %d..."%(pawn.name,i,len(self.collision)))
+                #Collision(sel
+        #    collision = client.simGetCollisionInfo(vehicle_name="Drone2")
+        #    if collision.has_collided:
+        #        printunrealengine NewMap is generated
   
 
-    # DAA run, break if detected, deem as fair detection or false alarm
-    dt=daa.start()
-    # Try catch here ???????
-    print("DAA started...")
+        # DAA run, break if detected, deem as fair detection or false alarm
+        daa_thr=daa.start()
+        # Try catch here ???????
+        print("DAA started...")
 
+        # Set pawn pose approach pitch and yaw,0
+        # client.simSetObjectPose("Pawn",pose,teleport=True)
+        #pt = client.moveToPositionAsync(20, 2, -10, 5, vehicle_name="Pawn",yaw_mode={'is_rate':False,'yaw_or_rate':45})
 
-    # Instead of using moveToPosition is may be better to update target position to deal with drift of drone
-    pt = client.moveToPositionAsync(pawn.end.x, pawn.end.y, pawn.end.z, pawn.v, vehicle_name="Pawn")
-   
-    # TODO - add enum to collision class!!!
-    detection=Detection.MISSED
-    while True:
-        dronepos = client.getMultirotorState(vehicle_name="Drone").kinematics_estimated.position
-        pawnpos = client.getMultirotorState(vehicle_name="Pawn").kinematics_estimated.position
-        #print("Pos: ",dronepos.x_val)
-        # Has Pawn reached collision position within radius?
-        # WARNING! : Take care as two meshes can be touching but their centers still far appart!
-       
-        # Better inside the class as it uses ditance
-        #can call update(x,y,z) then do collision.distanceto, hascollided=false
+        # Instead of using moveToPosition is may be better to update target position to deal with drift of drone
         
-        collision.update([dronepos.x_val,dronepos.y_val,dronepos.z_val],[pawnpos.x_val,pawnpos.y_val,pawnpos.z_val])
+        # Drone doesn't move yet?
+        #dt = cl.moveToPositionAsync(drone.start.x_val, drone.start.y_val, drone.start.z_val, drone.v, vehicle_name="Drone")    
+        pt = cl.moveToPositionAsync(pawn.end.x_val, pawn.end.y_val, pawn.end.z_val, pawn.v, vehicle_name=pawn.name, \
+                                  timeout_sec=pawn.timeout, yaw_mode={'is_rate':False,'yaw_or_rate':pawn.yaw})
+        #pt.join()
 
-        if collision.hascollided:  
+
+
+        # TODO - add enum to collision class!!!
+        detection=Detection.MISSED
+        while True:
+            dronepos = cl.getMultirotorState(vehicle_name="Drone").kinematics_estimated.position
+            pawnpos = cl.getMultirotorState(vehicle_name=pawn.name).kinematics_estimated.position
+            #print("Pos: ",dronepos.x_val)
+            # Has Pawn reached collision position within radius?
+            # WARNING! : Take care as two meshes can be touching but their centers still far appart!
+       
+            # Better inside the class as it uses distance?
+            #can call update(x,y,z) then do collision.distanceto, hascollided=false
+            self.update([dronepos.x_val,dronepos.y_val,dronepos.z_val],[pawnpos.x_val,pawnpos.y_val,pawnpos.z_val])
+
             # Missed detection,det=MISSED set above 
-            print("Pawn ended, killing DAA") 
-            # Stop DAA thread gracefully
-            daa.stop()
-            break
-        # Has DAA thread exited?
-        if not dt.is_alive():
-            # Stop pawn and reset, break and reset below
-            if collision.inrange:
-                print("DAA ended, killing pawn- - in range")
-            else:
-                print("DAA ended, killing pawn - out")
+            if self.hascollided:                
+                print("Pawn ended, killing DAA") 
+                # Stop DAA thread gracefully
+                daa.stop()
+                break
+
+            # Has DAA thread exited?
+            if not daa_thr.is_alive():
+                # Stop pawn and reset, break and reset below
+                if self.inrange:
+                    print("DAA ended, killing pawn- - in range")
+                else:
+                    print("DAA ended, killing pawn - out")
             
-            print("Dist: ",collision.distance)
-            detection = Detection.TRUE if collision.inrange else Detection.FALSE
-            break
+                print("Dist: ",self.distance)
+                detection = Detection.TRUE if self.inrange else Detection.FALSE
+                break
 
-        # Debug only as the delay skews the daa result
-        #time.sleep(1)
-
-
-
-    # Record result, det: 1, miss 0, fa    def pos3d_array():
-    #    return [self.x,self.y,self.z]lse alarm -1
-
-    # Todo: Python 3.10> has match-case to make this more C like
-    if Detection.MISSED == detection:
-        print("Missed Detection")
-    elif Detection.TRUE == detection:
-        print("True Detection")
-    else: #Detection.FALSE == detection:
-        print("False Detection")
+            # Debug only as the delay skews the daa result
+            #time.sleep(1)
 
 
-    drone=collision.drone
-    pawn=collision.pawn
 
-    #print("disarming and resetting...")
+        # Record result, det: 1, miss 0, fa 
+        # Todo: Python 3.10> has match-case to make this more C like
+        if Detection.MISSED == detection:
+            print("Missed Detection")
+        elif Detection.TRUE == detection:
+            print("True Detection")
+        else: #Detection.FALSE == detection:
+           print("False Detection")
 
-    client.armDisarm(False, "Drone")
-    client.armDisarm(False, "Pawn")
-    client.reset()
+        with open(RES_FILE,'a') as f:
+            f.write("%d %.2f {apr_ang=[%d,%d]}\n"%(detection.value, self.distance, pawn.approach[0], pawn.approach[1]))
+            #f.write("(%.1f,%.1f,%.1f) %d %.2f\n"%(pawn.end.x, pawn.end.y, pawn.end.z, detection.value, collision.distance))
 
-    # that's enough fun for now. let's quit cleanly
-    client.enableApiControl(False, "Drone")
-    client.enableApiControl(False, "Pawn")
+   
+        # Tidy up
+        cl.armDisarm(False, "Drone")
+        cl.armDisarm(False, pawn.name)
+        cl.enableApiControl(False, "Drone")
+        cl.enableApiControl(False, pawn.name)
+        cl.reset()
+
+        return 
 
 
-    return 
+
+
+
+
+# The collisions class holds the setup details and will step through different vehicle collisions
+class Collisions:
+
+    def __init__(self):
+
+        try:
+            self.client = airsim.MultirotorClient()
+            self.client.confirmConnection()
+        except Exception:
+            print("Could not connect to AirSim, is it running?")
+            raise Exception
+
+        # Load vehicle names and offsets from aimsim config file
+        # Create a inital dict of AirVehicle()
+        settings = json.loads(self.client.getSettingsString())
+        self.vehicle_settings={}
+        for k,v in settings['Vehicles'].items():
+            self.vehicle_settings[k] = v
+
+        # Load our simulation setup (not the airsim ones)
+        f = open(DEFAULT_SETUPS)
+        self.setup = json.load(f)['scenarios'] # Just scenario 1 for now
+        f.close()
+
+        # Good practice to deep copy settings as we delete it below!
+        self.drone = AirVehicle('Drone',copy.deepcopy(self.vehicle_settings['Drone'])) #,self.setup)
+
+        # Set drone trajectory from setup file
+        self.drone.setTrajectory(self.setup['dron_spos'],self.setup['dron_epos'],self.setup['dron_velc'])
+
+        # Set drone and drop 'Drone' from the dict as we can't avoid ourselves looping through in start()
+        del self.vehicle_settings['Drone']
+    
+
+        # Create a list of pawn vehicles with start and end points
+        self.collisions=[]
+
+        # TODO - Support for scenarios has not been included yet, do so here in the future
+        for name,settings in self.vehicle_settings.items():
+
+            for ang in self.setup['pawn_apr_ang']:
+                #print(self.setup['pawn_sdist'])
+                v=AirVehicle(name,settings,distance=self.setup['pawn_sdist'])
+                v.setTrajectory(self.get_appr_spos(ang,self.setup['pawn_sdist'],self.setup['pawn_epos']), \
+                            self.setup['pawn_epos'],self.setup['pawn_velc'],yaw=ang[0])
+                self.collisions.append(v)
+            break # DEBUG - REMOVE
+
+
+    # Seems best to have this here rather than overcomplicate AirVehicle
+    def get_appr_spos(self,a,r,e):
+      
+        # We are using the ISO 3D spherical coordinate system
+        # https://en.wikipedia.org/wiki/Spherical_coordinate_system
+        #
+        # looking ahead will be (90,90)
+        # to the right (0,90)
+        # Straight up (0,0)
+        # For PX4 we need to do an axis rotation swap x,y, invert z
+        # x,y swapped below to convert to PX4 coord system!
+        # Offsets WRT to (0,0,0)
+        azm,zen = a
+        yy =  r * math.cos(math.radians(azm)) * math.sin(math.radians(zen))
+        xx =  r * math.sin(math.radians(azm)) * math.sin(math.radians(zen))
+        zz = -r * math.cos(math.radians(zen))
+        # Shift WRT to global
+        x,y,z = e
+
+        return [round(x+xx,2),round(y+yy,2),round(z+zz,2)]
+
+
+    def start(self):
+
+            i=1
+            for pawn in self.collisions:
+                print("Running simulation for %s %d of %d..."%(pawn.name,i,len(self.collisions)))
+                c=Collision(self.setup)
+                c.start(self.client,self.drone,pawn)
+                i+=1
+                break
+        
+    # Collision range 50 > x > 5
+    #collision = Collision([90,90],setup['scenarios'])
+    #start(collision)
+    # put start in class then collis.start()
+
+        # Collide to air vehicles
+        #Collision()
+        #i=1
+        #for spos in pawn_spos:
+        #for apr_ang in setup['scenarios']['pawn_apr_ang']:
+        #    print("Running simulation %d of %d..."%(i,len(setup['scenarios']['pawn_apr_ang'])))
+        #    start(Collision(apr_ang,setup['scenarios']))
+        #    i+=1
+
+     
+
+
+
+
 
 
 # Main Setup
 if __name__ == '__main__':
     
-    # Collision range 50 > x > 5
-    collision = Collision(50,5)
-    collision.setDrone(WayPoint(Pos(-10,0, -20), Pos(-10, 0, -20),10))
-    collision.setPawn( WayPoint(Pos(50, 0, -20), Pos(-10, 0, -20),10))
-    start(collision)
+    c=Collisions()
+    c.start()
+    
+  
+
+
 
