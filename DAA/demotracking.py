2cb5c31e6542ece21659af4a0906096aaed94e5d
skytechltd <climber247@gmail.com>
2021-10-21 17:10:50 +0100

    Latest 21oct21

--- DAA/demotracking.py
+++ DAA/demotracking.py
@@ -9,7 +9,7 @@ import time
 import imutils
 import queue
 import copy
-from bboxmerge import merge_bboxes
+from bboxmerge import merge_bboxes, IOU
 
 #(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 
@@ -66,8 +66,34 @@ class DAA_Tracking:
         self.UFO_Count=self.UFO_Count-1
         
 
+
+    # Determine avoid action
+    def avoid_action(self,bbox,frame):
+
+        # Based on horz/vert centers issue avoid up,down,left,right
+        (x,y,w,h)=bbox
+        # Move Up
+        if y+(h/2) < self.cheight:
+            text="Move down"
+        # Move Up
+        elif y+(h/2) > self.cheight:
+            text="Move up"
+        # Move Up
+        elif x+(w/2) < self.cwidth:
+            text="Move right"
+        # Move Up
+        elif x+(w/2) > self.cwidth:
+            text="Move left"
+
+        print(text)
+
+        cv2.putText(frame, "Detection {}".format(text), (10, 20),
+		                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+   
+
     # Update all the trackers
-    def object_tracking(self,frame):
+    def object_tracking(self,currframe,frame):
 
 
         print("Tracking %d objects"%(len(self.UFOS)))
@@ -76,38 +102,59 @@ class DAA_Tracking:
 
 
             # Update tracker
-            ufo.ok, ufo.bbox = ufo.tracker.update(frame)
+            ok, bbox = ufo.tracker.update(currframe)
 
 
-            # Draw bounding box
-            if ufo.ok:
+            # Draw bounding box for debug
+            if ok:
                 # Tracking success, draw box
-                p1 = (int(ufo.bbox[0]), int(ufo.bbox[1]))
-                p2 = (int(ufo.bbox[0] + ufo.bbox[2]), int(ufo.bbox[1] + ufo.bbox[3]))
+                print("Tracking bbox (%d,%d,%d,%d)"%(bbox[0],bbox[1],bbox[2],bbox[3]))
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[2]), int(bbox[3]))
                 cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+
+                #input("Press return to terminate the program")
+
+
+               
+                # If area is above threshold object approaching!
+                area = (bbox[2]-bbox[0])*(bbox[3]-bbox[1])
+                #self.UFOS[i].history.put(ufo.area-area)
+                #print("Ave: ",self.history.ave())
+                print("Area:",int(area))
+                if area > self.UFOS[i].area*1.1:
+                    print("Change: ",area/self.UFOS[i].area)
+
+                    cv2.rectangle(frame, p1, p2, (0,0,255), 2, 1)
+                    cv2.putText(frame, "Collision risk!", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+
+                    self.avoid_action(bbox,frame)
+                else:
+                    cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+
+
+                self.UFOS[i].area = area
+
+
+
+
             else:
                 # Tracking failure
                 #cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
                 print("Tracking failed for ID, deleting")
-                self.delUFO(ufo)
+                #self.delUFO(ufo)
+                self.UFOS[i]=None
+            
 
 
-       
 
-        #print("BB: ",self.roi)
-        #print("Ave: ",self.history.ave())
-        # If area is above threshold object approaching!
-        #area = self.roi[2]*self.roi[3]
-        #elf.history.put(self.area-area)
-        #if area > self.area*1.2:
-            #print("Change: ",area/self.area)
-            #cv2.putText(frame, "Collision risk!", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+        # Remove deleted marked None
+        self.UFOS = [i for i in self.UFOS if i is not None]
 
-            #self.area = area
 
-        # If history is moving away then remove
-        #if self.history.ave() < 0:
-        #    print("Remove")
+            # If history is moving away then remove
+           # if ufo.history.ave() < 0:
+            #    print("Moving away - Remove tracking")
         #    input("Press return to terminate the program")
         #    return True
 
@@ -118,8 +165,8 @@ class DAA_Tracking:
 
 
         # Run every x seconds   
-        if self.frames < 25*5:
-            return  
+        #if self.frames < 25*5:
+        #    return  
         selfframes=0  
 
         framec=copy.copy(frame)
@@ -146,41 +193,41 @@ class DAA_Tracking:
 
             rects.append([x,y,x+w,y+h])
             
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
-        cv2.imshow('tracking', frame)
-        # Merge overlapping ROI
-        #rects,weights = cv2.groupRectangles(rects, 1, 2)
-        #print(rects)
+        #cv2.imshow('tracking', frame)
 
+        #return
 
+        # Merge overlapping ROI from detection
         rects = merge_bboxes(rects)
 
-        # Reject if inside or union of existing roi
 
-        # ??
+        # Reject if overlapping existing roi's, don't do any more merging!
+        for r in rects[:]:
+            for ufo in self.UFOS:
+                if IOU(ufo.bbox,r):
+                    rects.remove(r)
+
+
+                    
+
 
         # Ok its new so add
-        #self.addUFO(currframe,(x, y, w, h))
-     
-      
-        
-
-        
         for r in rects:
             #print(r)
-            #self.addUFO(currframe,r)
+            self.addUFO(currframe,r)
             (x1, y1, x2, y2) = r
-            cv2.rectangle(framec, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             #cv2.rectangle(framec, (0, 0), (200, 200), (255, 0, 0), 2)
            
 
-        cv2.imshow('tracking2', framec)
+        #cv2.imshow('tracking2', frame)
 
 
         #cv2.waitKey(100)
-        input("Press return to terminate the program")
+        #input("Press return to terminate the program")
 
     def run(self):
 
@@ -191,11 +238,14 @@ class DAA_Tracking:
         #cap = cv.VideoCapture('http://0.0.0.0:5000/video_feed') # Stream
         cap = cv2.VideoCapture('dronedirecthit.avi')
         #cap = cv2.VideoCapture('office2.avi')
+        cap = cv2.VideoCapture('colideandshake4.avi')
 
         # Video meta
         self.cwidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH )/2
         self.cheight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT )/2
         self.fps =  cap.get(cv2.CAP_PROP_FPS)
+
+        #print("Info: %dx%d %d FPS"%(self.cwidth,self.cheight,self.fps))
 
 
         grabbed, frame = cap.read()
@@ -227,16 +277,16 @@ class DAA_Tracking:
 
   
             # Object Tracking
-            #self.object_tracking(gray)
-            print("Tracking %d objects"%(len(self.UFOS)))
+            self.object_tracking(currframe,frame)
+            #print("Tracking %d objects"%(len(self.UFOS)))
 
             # Object Detection
             self.object_detection(lastframe,currframe,frame)
 
 
             # Show Frame
-            #cv2.imshow('tracking', frame)
-            time.sleep(0.05)
+            cv2.imshow('tracking', frame)
+            time.sleep(0.1)
             if flag:
                 flag=False
                 #input("Press return to terminate the program")
