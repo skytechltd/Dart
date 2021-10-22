d5217bdec1b45ede2143fcb1dce40349abd147ee
skytechltd <climber247@gmail.com>
2021-10-22 15:07:15 +0100

    new bugsO

--- DAA/demotracking.py
+++ DAA/demotracking.py
@@ -38,7 +38,7 @@ class DAA_Tracking:
     class UFO:
         def __init__(self,bbox):
 
-            self.bbox = bbox
+            self.bbox = bbox # bbox formay x1,y1,x2,y2
             self.tracker = cv2.legacy.TrackerMedianFlow_create()
             self.area = self.bbox[2]*self.bbox[3]
             self.history = HQueue(25)
@@ -55,12 +55,11 @@ class DAA_Tracking:
     def addUFO(self,frame,bbox):
 
         ufo = self.UFO(bbox)
-        ufo.ok = ufo.tracker.init(frame,bbox)
+        ufo.ok = ufo.tracker.init(frame,bbox) # tracker accepts x1,y1,x2,y2 format not x,y,w,h as docs say?
         # Only add if tracking successful
         if ufo.ok:
             self.UFOS.append(ufo)
-
-
+      
     def delUFO(self,ufo):
         self.UFOS.remove(ufo)
         self.UFO_Count=self.UFO_Count-1
@@ -111,19 +110,23 @@ class DAA_Tracking:
                 print("Tracking bbox (%d,%d,%d,%d)"%(bbox[0],bbox[1],bbox[2],bbox[3]))
                 p1 = (int(bbox[0]), int(bbox[1]))
                 p2 = (int(bbox[2]), int(bbox[3]))
-                cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+                #p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                #cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
 
                 #input("Press return to terminate the program")
 
-
+                # TODO - these values are questionable? The bbox format in the docs is x,y,w,h
+                # But x,y,x1,y1 seems to print correctly
+                # So is the tracker just dodgy that it makes such a big change?
                
                 # If area is above threshold object approaching!
                 area = (bbox[2]-bbox[0])*(bbox[3]-bbox[1])
                 #self.UFOS[i].history.put(ufo.area-area)
                 #print("Ave: ",self.history.ave())
-                print("Area:",int(area))
+                #print("Area:",int(area))
+
                 if area > self.UFOS[i].area*1.1:
-                    print("Change: ",area/self.UFOS[i].area)
+                    #print("Change: ",area/self.UFOS[i].area)
 
                     cv2.rectangle(frame, p1, p2, (0,0,255), 2, 1)
                     cv2.putText(frame, "Collision risk!", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
@@ -131,11 +134,10 @@ class DAA_Tracking:
                     self.avoid_action(bbox,frame)
                 else:
                     cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+                    pass
 
 
                 self.UFOS[i].area = area
-
-
 
 
             else:
@@ -151,6 +153,8 @@ class DAA_Tracking:
         # Remove deleted marked None
         self.UFOS = [i for i in self.UFOS if i is not None]
 
+
+        #input("Press return to terminate the program")
 
             # If history is moving away then remove
            # if ufo.history.ave() < 0:
@@ -169,10 +173,10 @@ class DAA_Tracking:
         #    return  
         selfframes=0  
 
-        framec=copy.copy(frame)
+        #framec=copy.copy(frame)
 
         frameDelta = cv2.absdiff(lastframe, currframe)
-        #frameDelta = cv2.GaussianBlur(frameDelta, (21, 21), 0)
+        frameDelta = cv2.GaussianBlur(frameDelta, (21, 21), 0)
         thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
 	    # dilate the thresholded image to fill in holes, then find contours
@@ -185,49 +189,58 @@ class DAA_Tracking:
 	    # loop over the contours and assign rects
         for c in cnts:
             # if the contour is too small, ignore it
-            if cv2.contourArea(c) < 50: #self.args["min_area"]:
+            if cv2.contourArea(c) < 500: #self.args["min_area"]:
                 continue
 		    # compute the bounding box for the contour, draw it on the frame,
 		    # and update the text
             (x, y, w, h) = cv2.boundingRect(c)
 
-            rects.append([x,y,x+w,y+h])
+            if(w<200,h<200):
+                rects.append([x,y,x+w,y+h])
             
-            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+            print("aasds: %d,%d,%d,%d"%(x, y, w, h))
 
-        #cv2.imshow('tracking', frame)
+        #input("Press return to terminate the program")
+            
 
-        #return
 
         # Merge overlapping ROI from detection
         rects = merge_bboxes(rects)
 
 
-        # Reject if overlapping existing roi's, don't do any more merging!
-        for r in rects[:]:
-            for ufo in self.UFOS:
-                if IOU(ufo.bbox,r):
+
+        # Reject if overlapping existing roi's partially
+        for ufo in self.UFOS:
+            for r in rects[:]: # TODO Any copy weirdness if we are deleting this way?
+                t,f = IOU(ufo.bbox,r,-50)
+                if t:
+                    print("Deleting Rect CP: (%d,%d,%d,%d)-(%d,%d,%d,%d)"%(ufo.bbox[0],ufo.bbox[1],ufo.bbox[2],ufo.bbox[3],r[0],r[1],r[2],r[3]))
                     rects.remove(r)
+                    #(x1, y1, x2, y2) = ufo.bbox
+                    #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
+                    #(x1, y1, x2, y2) = r
+                    #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    
+        # Likewise if a detection is overlapping a tracking region and is clearly better replace
 
-
+        # ???????????????
                     
 
 
         # Ok its new so add
         for r in rects:
-            #print(r)
-            self.addUFO(currframe,r)
+            print("Creating new tracker: ",r)
+            self.addUFO(currframe,(r[0],r[1],(r[2]-r[0]),(r[3]-r[1])))
             (x1, y1, x2, y2) = r
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             #cv2.rectangle(framec, (0, 0), (200, 200), (255, 0, 0), 2)
-           
+        
+     
+            #input("Press return to terminate the program")
 
-        #cv2.imshow('tracking2', frame)
 
-
-        #cv2.waitKey(100)
-        #input("Press return to terminate the program")
 
     def run(self):
 
@@ -238,14 +251,14 @@ class DAA_Tracking:
         #cap = cv.VideoCapture('http://0.0.0.0:5000/video_feed') # Stream
         cap = cv2.VideoCapture('dronedirecthit.avi')
         #cap = cv2.VideoCapture('office2.avi')
-        cap = cv2.VideoCapture('colideandshake4.avi')
+        #cap = cv2.VideoCapture('colideandshake4.avi')
 
         # Video meta
         self.cwidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH )/2
         self.cheight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT )/2
         self.fps =  cap.get(cv2.CAP_PROP_FPS)
 
-        #print("Info: %dx%d %d FPS"%(self.cwidth,self.cheight,self.fps))
+        print("Info: %dx%d %d FPS"%(self.cwidth,self.cheight,self.fps))
 
 
         grabbed, frame = cap.read()
@@ -260,6 +273,7 @@ class DAA_Tracking:
         # Loop over frames
         while True:
 
+
             # Pause/Unpause if space is pressed
             key = cv2.waitKey(1) & 0xFF
             #if cv.waitKey(1) & 0xFF == ord(' '):
@@ -267,6 +281,8 @@ class DAA_Tracking:
             if key == ord("q"):
                 break
 
+            # Start timer
+            timer = cv2.getTickCount()
 
             # Grab next frame
             grabbed, frame = cap.read()
@@ -280,8 +296,16 @@ class DAA_Tracking:
             self.object_tracking(currframe,frame)
             #print("Tracking %d objects"%(len(self.UFOS)))
 
+
             # Object Detection
             self.object_detection(lastframe,currframe,frame)
+
+
+            # Calculate Frames per second (FPS)
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+            # Display FPS on frame
+            #cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
 
 
             # Show Frame
@@ -312,14 +336,17 @@ def unittest_boxmerge():
 
     #rects=[[20,20,40,40],[1,1,25,25],[0,0,3,3],[39,39,50,50]]
 
-    rects=[[0,0,100,100],[10,10,50,50]]
+    #rects=[[0,0,100,100],[10,10,50,50]]
+
+    rects=[[241,76,272,90],[229,35,286,66]]
+
 
     img = cv2.imread('testgrid.png')
     img2 = cv2.imread('testgrid.png')
 
     for r in rects:
-        (x,y,w,h) = r
-        cv2.rectangle(img, (x, y), (w, h), (255, 0, 0), 2)
+        (x1,y1,x2,y2) = r
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
     cv2.imshow('test', img)
     #rects = non_max_suppression_fast(np.array(rects),0.1)
@@ -329,10 +356,8 @@ def unittest_boxmerge():
     rects = merge_bboxes(rects)
 
     for r in rects:
-        (x,y,w,h) = r
-        cv2.rectangle(img2, (x, y), (w, h), (0, 0, 255), 2)
-
-
+        (x1,y1,x2,y2) = r
+        cv2.rectangle(img2, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     cv2.imshow('test1', img2)
 
