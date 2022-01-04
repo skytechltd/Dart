43e58d4cd91cd93c74c0d62093c8fc5491a339e5
skytechltd <climber247@gmail.com>
2022-01-04 16:37:11 +0000

    Removed old code

--- Benchmarking/showres.py
+++ Benchmarking/showres.py
@@ -1,4 +1,20 @@
 
+#
+# Copyright 2022 David Redpath - Sky Tech Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 #
 # Read results file an calculate error, precision and recall
@@ -7,14 +23,19 @@
 #
 # Due to a set of inputs
 #
-#
-#
 
-RES_FILE='daares.csv'
 
+
+import sys
 import numpy as np
 from numpy import genfromtxt
-rawres = genfromtxt(RES_FILE, delimiter=' ')
+
+
+if len(sys.argv)<=1:
+    print("No filepath entered")
+    sys.exit()
+
+rawres = genfromtxt(sys.argv[1], delimiter=' ')
 
 #print(rawres)
 
@@ -25,8 +46,11 @@ FN=np.count_nonzero(rawres[:,0] == 0)/r
 FP=np.count_nonzero(rawres[:,0] == -1)/r
 P=TP/(TP+FP) if TP > 0 else 0
 R=TP/(TP+FN)
-print("Accuracy: %.2f, Precision: %.2f, Recall %.2f\n"%(TP,P,R))
+ADD=np.average(rawres[:,1]) # Average det distance
 
+#print("Accuracy: %.2f, FP: %.2f, ADD: %.2f, Precision: %.2f, Recall %.2f\n"%(TP,FP,ADD,P,R))
+
+print("Accuracy: %.2f, MD: %.2f, FP: %.2f, AR: %.2f"%(TP,FN,FP,ADD))
 
 # Any other info we can glean?
 
